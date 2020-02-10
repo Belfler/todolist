ea8072fa6fcbe8a23fe8caa71ed3a15fd243ccf9
Alexander Khoroshavin <neutok@gmail.com>
2020-02-10 23:19:46 +0300

    Add TestIndexPage class
    For a while test that 'Django' is in title of page

--- core/tests.py
+++ core/tests.py
@@ -1,1 +1,15 @@
 from django.test import TestCase
+
+from selenium import webdriver
+
+
+class TestIndexPage(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_word_django_is_in_title(self):
+        self.browser.get('http://localhost:8000')
+        assert 'Django' in self.browser.title
