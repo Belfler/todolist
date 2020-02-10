19eeca41ff889580b5491e2a4fd8a51852eaa76d
Alexander Khoroshavin <neutok@gmail.com>
2020-02-11 01:13:06 +0300

    Add IndexView

--- core/tests.py
+++ core/tests.py
@@ -1,15 +1,17 @@
-from django.test import TestCase
-
 from selenium import webdriver
+from django.test import TestCase
+from django.urls import resolve
+
+from core.views import IndexView
 
 
 class TestIndexPage(TestCase):
-    def setUp(self):
-        self.browser = webdriver.Chrome()
-
-    def tearDown(self):
-        self.browser.quit()
-
     def test_word_django_is_in_title(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('Django', self.browser.title)
+        browser = webdriver.Chrome()
+        browser.get('http://localhost:8000')
+        self.assertIn('Index page', browser.page_source)
+        browser.quit()
+
+    def test_root_url_resolves_to_index_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func.__name__, IndexView.as_view().__name__)
--- core/views.py
+++ core/views.py
@@ -1,1 +1,9 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+__all__ = ['IndexView']
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Index page')
